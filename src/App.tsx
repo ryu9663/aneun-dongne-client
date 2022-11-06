10bc98a5be47ef35d09bf3a1ac00f8444b0a935a
june <66232436+ryu9663@users.noreply.github.com>
2022-11-06 15:38:35 +0900

    feat : SKSK-4 헤더 UI 생성 (#3)
    * feat : SKSK-4 헤더 UI 생성
    
    * fix : 랜딩페이지에서 스크롤 버튼 눌렸을 때 화면 내려가는 깊이 조정
    
    * fix : 랜딩페이지 스크롤 화살표 스타일 조정
    
    Co-authored-by: junyeol.ryu <junyeol.ryu@junyeolryu.local>

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Header from 'components/Header';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/main';
@@ -5,6 +6,7 @@ import Main from './pages/main';
 function App() {
   return (
     <>
+      <Header />
       <h1 className={'section_title_hidden'}>아는동네</h1>
       {/* <Header handleResponseSuccess={handleResponseSuccess} /> */}
       <Routes>
