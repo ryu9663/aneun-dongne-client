429a41ea5592bc48a28474d3932b8f4090a90003
junyeol.ryu <junyeol.ryu@junyeolryu.local>
2022-11-19 17:10:46 +0900

    feature : SKSK-9 TourAPI 연동 (#5)

---
+++ src/pages/mappage/types.ts
@@ -0,0 +1,29 @@
+export interface PlaceParams {
+  numOfRows: number;
+  mapX?: number;
+  mapY?: number;
+  radius: number;
+}
+export interface PlaceType {
+  addr1: string;
+  addr2: string;
+  areacode: string;
+  booktour: string;
+  cat1: string;
+  cat2: string;
+  cat3: string;
+  contentid: string;
+  contenttypeid: string;
+  createdtime: string;
+  dist: string;
+  firstimage: string;
+  firstimage2: string;
+  mapx: string;
+  mapy: string;
+  mlevel: string;
+  modifiedtime: string;
+  readcount: string;
+  sigungucode: string;
+  tel: string;
+  title: string;
+}
