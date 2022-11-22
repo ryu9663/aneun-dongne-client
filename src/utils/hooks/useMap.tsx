a524d691a767c26cbf39403aa83d8d654e330dcc
junyeol.ryu <junyeol.ryu@junyeolryu.local>
2022-11-22 22:56:03 +0900

    tourAPI isLoading 추가

--- src/utils/hooks/useMap.tsx
+++ src/utils/hooks/useMap.tsx
@@ -1,16 +1,22 @@
 import { addZoomControler, setOtherMarkers } from 'components/KakaoMap/handler';
+import { PlaceType } from 'pages/mappage/types';
 import { useEffect, useRef } from 'react';
+import { PositionType } from './useCurrentPosition';
 
-const useMap = (mapRef: any, otherMarkers?: any) => {
+const useMap = (mapRef: any, otherMarkers?: PlaceType[], currentPosition?: PositionType) => {
   const kakao = window.kakao;
   const kakaoMap = useRef();
 
   useEffect(() => {
     if (mapRef.current) {
+      console.log(currentPosition);
       const container = mapRef.current;
+      const center = new kakao.maps.LatLng(
+        currentPosition ? currentPosition.lat : 37.1597041,
+        currentPosition ? currentPosition.lon : 128.213384
+      );
       const option = {
-        center: new kakao.maps.LatLng(37.1597041, 128.213384),
-
+        center,
         level: 8
       };
       kakaoMap.current = new kakao.maps.Map(container, option);
