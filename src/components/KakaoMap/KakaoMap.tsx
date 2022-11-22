48aee8d6fe1f99ecb97186e851f8cde567620231
junyeol.ryu <junyeol.ryu@junyeolryu.local>
2022-11-22 15:02:28 +0900

    useMap 생성

--- src/components/KakaoMap/KakaoMap.tsx
+++ src/components/KakaoMap/KakaoMap.tsx
@@ -7,6 +7,7 @@ import { PlaceType } from 'pages/mappage/types';
 import { useQuery } from '@tanstack/react-query';
 import queryKeys from 'query/queryKeys';
 import { getPlaces } from 'query/queryFn';
+import useMap from 'utils/hooks/useMap';
 
 export interface Props {
   currentPosition?: PositionType;
@@ -16,29 +17,9 @@ export interface Props {
   setPickPoint: (position?: PositionType) => void;
 }
 
-// const KakaoMap = ({ currentPosition, places, pickPoint, setPickPoint}: Props) => {
 const KakaoMap = ({ currentPosition, pickPoint, setPickPoint }: Props) => {
   const mapRef = useRef(null);
-  const kakaoMap = useRef();
-  const kakao = window.kakao;
-
-  useEffect(() => {
-    if (currentPosition) {
-      const container = mapRef.current;
-      const option = {
-        center: new kakao.maps.LatLng(
-          pickPoint ? pickPoint.lat : currentPosition?.lat,
-          pickPoint ? pickPoint.lon : currentPosition?.lon
-        ),
-        level: 8
-      };
-      kakaoMap.current = new kakao.maps.Map(container, option);
-      addZoomControler(kakaoMap.current);
-      setPickPoint && onDragMap(kakaoMap.current, setPickPoint);
-    }
-  }, [mapRef]);
-
-  //! TODO : 쿼리를 포함하는 useMap 훅을 하나 만들어야함
+  //! TODO : 쿼리를 포함하는 useMap 훅을 하나 만들거나, 지금처럼 data를 useMap안에 넣던가
   const placeParmas = useMemo(
     () => ({
       numOfRows: 50,
@@ -56,15 +37,18 @@ const KakaoMap = ({ currentPosition, pickPoint, setPickPoint }: Props) => {
 
   const places = placesData?.response?.body?.items.item;
   //!
+  const { map: kakaoMap } = useMap(mapRef, places);
 
   useEffect(() => {
-    if (places) setOtherMarkers(kakaoMap.current, places);
-  }, [places]);
+    if (kakaoMap.current) setPickPoint && onDragMap(kakaoMap.current, setPickPoint);
+  }, [kakaoMap]);
 
   return (
     <>
-      <div className="map-experiment">&nbsp;&nbsp;{'마커를 클릭하시면 해당 문화재 검색창으로 이동합니다.'}</div>
-      <article ref={mapRef} className={styles.map} id={styles.map}></article>
+      <>
+        <div className="map-experiment">&nbsp;&nbsp;{'마커를 클릭하시면 해당 문화재 검색창으로 이동합니다.'}</div>
+        <article ref={mapRef} className={styles.map} id={styles.map}></article>
+      </>
     </>
   );
 };
