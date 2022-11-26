d1ec357c5674aba3871d515d42723fe20b6b2b57
junyeol.ryu <junyeol.ryu@junyeolryu.local>
2022-11-27 04:50:03 +0900

    header 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Link to="/">
-        <img src="/images/logo.png" id={styles.logo} alt="logo" width={100} height={50} />
-      </Link>
+      <div className={styles.info}>관광지 카드 클릭 후 생성되는 안내창을 닫는 기능은 개발예정입니다.</div>
       <div className={styles.header_button_wrapper}>
-        <button>로그인</button>
-        <button>회원가입</button>
+        <button>로그인은 필요 없지</button>
+        <button>회원가입 역싀 필요 없지</button>
       </div>
     </header>
   );
