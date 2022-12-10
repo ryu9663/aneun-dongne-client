17702c0c5834ea0d5cbcfc9d089232696a658490
junyeol.ryu <junyeol.ryu@junyeolryu.local>
2022-12-10 22:23:14 +0900

    feat : SKSK-21 관광공사에 등록되지 않은 이미지 표시

--- src/pages/mappage/components/PlaceList/Place/Place.tsx
+++ src/pages/mappage/components/PlaceList/Place/Place.tsx
@@ -11,7 +11,11 @@ const Place = ({ title, addr1, firstimage, onMouseEnter }: Props) => {
   return (
     <div className={styles.wrapper} onMouseEnter={() => onMouseEnter(title)}>
       <span className={styles.wrapper_title}>{title}</span>
-      <img className={styles.wrapper_img} src={firstimage} alt={title} />
+      <img
+        className={styles.wrapper_img}
+        src={firstimage || '/images/no-image.png'}
+        alt={firstimage ? title : '관광공사에 관광지의 사진이 등록되지 않았습니다.'}
+      />
       <p className={styles.addr1}>{addr1}</p>
     </div>
   );
