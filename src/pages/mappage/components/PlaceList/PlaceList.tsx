f7049996719e5c3bcba3ecd69915f70822ddbd32
junyeol.ryu <junyeol.ryu@junyeolryu.local>
2022-12-10 16:17:49 +0900

    fix : SKSK-20 특정상황에서 마커,인포가 초기화되지 않는 버그
    stricmode가 없는 환경에서도 마커,인포가 업데이트 되도록 수정

--- src/pages/mappage/components/PlaceList/PlaceList.tsx
+++ src/pages/mappage/components/PlaceList/PlaceList.tsx
@@ -1,5 +1,5 @@
-import { PlaceType } from 'pages/mappage/types';
-import { useState } from 'react';
+import { MarkerType, PlaceType } from 'pages/mappage/types';
+import { useEffect } from 'react';
 import { removeInfo, showSelectedPlacesInfo } from 'utils/handleMapMarkers';
 
 import Place from './Place/Place';
@@ -8,10 +8,12 @@ import styles from './placelist.module.scss';
 interface Props {
   places: PlaceType[];
   map: any;
+  prevMarkers?: MarkerType[];
+  setPrevMarkers?: (markers: MarkerType[]) => void;
+  prevInfo: any;
+  setPrevInfo: any;
 }
-const PlaceList = ({ places, map }: Props) => {
-  const [prevInfo, setPrevInfo] = useState<any>();
-
+const PlaceList = ({ places, map, prevMarkers, prevInfo, setPrevInfo, setPrevMarkers }: Props) => {
   const onHoverCards = (title: string) => {
     if (prevInfo) {
       removeInfo(prevInfo);
@@ -20,12 +22,18 @@ const PlaceList = ({ places, map }: Props) => {
     const clickedCards = places.find(place => place.title === title);
 
     if (clickedCards) {
-      setPrevInfo(showSelectedPlacesInfo([clickedCards], map.current)[0]);
-      showSelectedPlacesInfo([clickedCards], map.current)[0].close();
+      setPrevInfo(showSelectedPlacesInfo([clickedCards], map.current));
     } else {
       alert('지도에서 찾지 못한 카드입니다. 개발자에게 문의하세요');
     }
   };
+
+  useEffect(() => {
+    return () => {
+      prevInfo && removeInfo(prevInfo);
+    };
+  }, [prevInfo]);
+
   return (
     <section className={styles.wrapper}>
       <div className={styles.cards}>
