2a727d5332b6b03c0927803f0705427e3132cc39
june <ryu9663@naver.com>
2022-12-25 21:54:38 +0900

    feat : SKSK-23 관광지 세부페이지 UI

--- src/query/queryKeys.ts
+++ src/query/queryKeys.ts
@@ -1,9 +1,10 @@
 import { PlaceParams } from '../pages/mappage/types';
 
 const PLACES = 'places';
-
+const PLACE = 'place';
 const queryKeys = {
-  PLACES: (params: PlaceParams) => [PLACES, params.mapX || 0, params.mapY || 0, params.radius] as const
+  PLACES: (params: PlaceParams) => [PLACES, params.mapX || 0, params.mapY || 0, params.radius] as const,
+  PLACE: (contentId: string) => [PLACE, contentId] as const
 };
 
 export default queryKeys;
