ce66f1a3f1248f40ee32599915682a7b05cfae5b
june <ryu9663@naver.com>
2022-12-29 20:31:58 +0900

    feat : SKSK-28 관광지 세부페이지 반응형

--- src/pages/mappage/components/PlaceList/Place/Place.tsx
+++ src/pages/mappage/components/PlaceList/Place/Place.tsx
@@ -1,5 +1,4 @@
 import styles from './place.module.scss';
-import { StyledLink } from 'utils/StyledLink';
 
 interface Props {
   title: string;
@@ -11,17 +10,21 @@ interface Props {
 
 const Place = ({ title, addr1, firstimage, onMouseEnter, contentId }: Props) => {
   return (
-    <StyledLink to={`/detailpage/${contentId}`}>
-      <div className={styles.wrapper} onMouseEnter={() => onMouseEnter(title)}>
-        <span className={styles.wrapper_title}>{title}</span>
-        <img
-          className={styles.wrapper_img}
-          src={firstimage || '/images/no-image.png'}
-          alt={firstimage ? title : '관광공사에 관광지의 사진이 등록되지 않았습니다.'}
-        />
-        <p className={styles.addr1}>{addr1}</p>
-      </div>
-    </StyledLink>
+    <a
+      className={styles.wrapper}
+      onMouseEnter={() => onMouseEnter(title)}
+      href={`/detailpage/${contentId}`}
+      rel="noreferrer"
+      target="_blank"
+    >
+      <span className={styles.wrapper_title}>{title}</span>
+      <img
+        className={styles.wrapper_img}
+        src={firstimage || '/images/no-image.png'}
+        alt={firstimage ? title : '관광공사에 관광지의 사진이 등록되지 않았습니다.'}
+      />
+      <p className={styles.addr1}>{addr1}</p>
+    </a>
   );
 };
 
