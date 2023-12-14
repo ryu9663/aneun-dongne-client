b1c1f8dfa7c4e029576f96c34e90f7ba56cb17b0
june <ryu9663@naver.com>
2023-12-14 14:57:11 +0900

    feat: 마커 클릭시 네이버 검색창으로 이동하도록 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,9 @@
 import styles from './header.module.scss';
-import { StyledLink } from 'utils/StyledLink';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={`${styles.info} `}>
-        <StyledLink to="/">
-          <button className={styles.button}>홈으로</button>
-        </StyledLink>
-      </div>
-      <div className={styles.header_button_wrapper}>
-        <button className={styles.button}>로그인은 필요 없지</button>
-        <button className={styles.button}>회원가입 역싀 필요 없지</button>
-      </div>
+      <h1 className={`${styles.title} `}>위치기반 관광지 찾아보기</h1>
     </header>
   );
 };
