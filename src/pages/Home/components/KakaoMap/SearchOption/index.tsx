0fac448afa8a58bee8d5e629f90a451c9d28a3c7
june <ryu9663@naver.com>
2023-12-15 14:42:28 +0900

    dropdown tag 적용

--- src/pages/Home/components/KakaoMap/SearchOption/index.tsx
+++ src/pages/Home/components/KakaoMap/SearchOption/index.tsx
@@ -1,46 +1,40 @@
-import { Button, Input, Modal, Selectbox } from 'junyeol-components';
+import { Input, OptionTag, Selectbox } from 'junyeol-components';
 import styles from './index.module.scss';
 import { useState } from 'react';
-import { isNumber } from 'lodash-es';
 
 export const SearchOption = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const [radius, setRadius] = useState('');
 
   return (
     <div className={styles.search_options}>
-      <Button onClick={() => setIsOpen(true)}>반경, 검색지 수 조정</Button>
-      <Modal size="large" onOk={() => setIsOpen(false)} isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <Modal.Header>헤더</Modal.Header>
-        <Modal.Body>
-          <div className={styles.modal_body}>
-            <Selectbox
-              name="관광지 검색 수"
-              size="medium"
-              onChange={v => console.log(v)}
-              options={[
-                { name: '10개', value: 10 },
-                { name: '20개', value: 20 },
-                { name: '30개', value: 30 },
-                { name: '40개', value: 40 },
-                { name: '50개', value: 50 }
-              ]}
-            />
+      <OptionTag name="관광지 검색 옵션 조정" className={`${styles.button} ${styles['font-weight-700']}`}>
+        <div>
+          <Selectbox
+            name="관광지 검색 수"
+            size="medium"
+            onChange={v => console.log(v)}
+            options={[
+              { name: '10개', value: 10 },
+              { name: '20개', value: 20 },
+              { name: '30개', value: 30 },
+              { name: '40개', value: 40 },
+              { name: '50개', value: 50 }
+            ]}
+          />
 
-            <Input
-              type="text"
-              value={radius}
-              onChange={e => setRadius(e.target.value)}
-              validation={value => {
-                console.log(typeof value, Number(value));
-                return isNaN(Number(value)) ? '숫자만 입력해주세요' : '';
-              }}
-              label={{ htmlFor: 'radius', name: '반경' }}
-              placeholder="반경(m)"
-            />
-          </div>
-        </Modal.Body>
-      </Modal>
+          <Input
+            type="text"
+            value={radius}
+            onChange={e => setRadius(e.target.value)}
+            validation={value => {
+              console.log(typeof value, Number(value));
+              return isNaN(Number(value)) ? '숫자만 입력해주세요' : '';
+            }}
+            label={{ htmlFor: 'radius', name: '반경' }}
+            placeholder="반경(m)"
+          />
+        </div>
+      </OptionTag>
     </div>
   );
 };
