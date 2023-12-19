0c21ebeda52815d14abde425b86f3f196a01086c
june <ryu9663@naver.com>
2023-12-19 19:48:01 +0900

    error 알림

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import Alert from 'components/Alert';
 import { Home } from 'pages/Home';
 import { useState } from 'react';
 import 'styles/_global.scss';
-import { detectDevice } from 'utils/detectDevice';
 
 function App() {
-  const device = detectDevice();
-  const [showDeviceAlert, setShowDeviceAlert] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <>
-      {device !== 'PC' && (
-        <Alert alertOn={showDeviceAlert} setAlertOn={setShowDeviceAlert} content="PC로 사용하시는 것을 추천드립니다." />
-      )}
+      <Alert alertOn={isOpen} setAlertOn={setIsOpen} content="정부 Open API 에러로 현재 이용이 불가합니다." />
+
       <h1>떠나요</h1>
       <main>
         <Home />
