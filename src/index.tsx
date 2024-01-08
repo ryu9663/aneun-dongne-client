5d64f701c697b121c7b65bb4eb2ad441f03e7d87
june <ryu9663@naver.com>
2024-01-08 12:02:55 +0900

    이게 맞는지 확신은 안가지만 일단 tsconfig.json 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import 'junyeol-components/style.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient({
@@ -24,6 +23,7 @@ const queryClient = new QueryClient({
     }
   }
 });
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
@@ -31,8 +31,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
