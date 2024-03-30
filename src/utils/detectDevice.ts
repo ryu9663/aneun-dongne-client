7f22b9c4d28022f5d9e53aeb7b9715a6ac8203b0
june <ryu9663@naver.com>
2024-03-30 17:48:30 +0900

    build error

--- src/utils/detectDevice.ts
+++
@@ -1,9 +0,0 @@
-import { isBrowser, isMobile } from 'react-device-detect';
-
-export const detectDevice = () => {
-  if (isBrowser) {
-    return 'PC';
-  } else if (isMobile) {
-    return 'Mobile';
-  } else return 'Tablet';
-};
