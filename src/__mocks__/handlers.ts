a0a06598145ed91c871604667368d066a32e1458
june <ryu9663@naver.com>
2024-04-21 19:03:27 +0900

    refactor: SKSK-49 테스트 코드 작성

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,1 +1,22 @@
-export const handlers = [];
+import { response1, response2 } from '@/__mocks__/constant';
+import { http, HttpResponse } from 'msw';
+
+const API_URL = `${import.meta.env.VITE_APP_TOUR_API_URL}/locationBasedList1?ServiceKey=${
+  import.meta.env.VITE_APP_TOUR_API_KEY
+}&_type=json`;
+
+export const handlers = [
+  http.get(API_URL, ({ request }) => {
+    const url = new URL(request.url);
+    const mapX = url.searchParams.get('mapX');
+    const mapY = url.searchParams.get('mapY');
+    const radius = url.searchParams.get('radius');
+    const numOfRows = url.searchParams.get('numOfRows');
+
+    if (mapX === '126.9637296' && mapY === '37.4737835' && radius === '10000' && numOfRows === '20') {
+      return HttpResponse.json(response1, { status: 200 });
+    } else {
+      return HttpResponse.json(response2, { status: 200 });
+    }
+  })
+];
