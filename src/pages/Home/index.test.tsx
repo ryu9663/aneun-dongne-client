9b7789b59b855f06c74e7a0debec2ce6619df7d6
june <ryu9663@naver.com>
2024-05-19 16:04:01 +0900

    feat: SKSK-59 카카오맵 마커 관련 모킹

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -5,10 +5,10 @@ import KakaoMap from '@/pages/Home/components/KakaoMap';
 import PlaceList from '@/pages/Home/components/PlaceList';
 import { PlaceType } from '@/pages/Home/types';
 import { renderAppWithRouterMatch } from '@/utils/test/renderWidthQuery';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-describe('홈 화면', () => {
-  it("위치추적 안되었을때 '현재 위치를 받아오고 있다는 문구' 노출", () => {
+describe('위치를 모를ㅎ때', () => {
+  test("위치추적 안되었을때 '현재 위치를 받아오고 있다는 문구' 노출", () => {
     render(renderAppWithRouterMatch(<Home />));
     const loadingTextForGettingPosition = screen.getByText('현재 위치를 받아오고 있습니다.');
     expect(loadingTextForGettingPosition).toBeInTheDocument();
@@ -16,7 +16,7 @@ describe('홈 화면', () => {
 });
 
 describe('위치를 알 때 지도', () => {
-  it('위치를 지도페이지에 넣으면 처음에 로딩이 뜬다.', async () => {
+  test('위치를 지도페이지에 넣으면 처음에 로딩이 뜬다.', async () => {
     render(
       renderAppWithRouterMatch(
         <HomeWithPosition
@@ -31,7 +31,7 @@ describe('위치를 알 때 지도', () => {
     expect(loading).toBeInTheDocument();
   });
 
-  it('api로 받은 장소들이 지도 하단에 카드목록으로 노출된다.', async () => {
+  test('api로 받은 장소들이 지도 하단에 카드목록으로 노출된다.', async () => {
     render(<PlaceList places={response1.response.body.items.item as unknown as PlaceType[]} />);
     const placeNames = response1.response.body.items.item.map(place => place.title);
 
@@ -43,7 +43,7 @@ describe('위치를 알 때 지도', () => {
     });
   });
 
-  it('지도가 렌더링되면 kakao-map을 확인할 수 있다.', () => {
+  test('지도가 렌더링되면 kakao-map을 확인할 수 있다.', () => {
     render(
       <KakaoMap
         position={{
@@ -57,10 +57,65 @@ describe('위치를 알 때 지도', () => {
     const kakaoMap = screen.getByTestId('kakao-map');
     expect(kakaoMap).toBeInTheDocument();
   });
-  // it('지도가 렌더링되면 마커들이 보인다.', () => {
-  //   // TODO 모킹함수로 마커를 가상돔에 렌더링시켜야함..
-  // });
-  // it('마커를 호버하면 장소의 정보가 보인다.', async () => {});
+  test('지도위에 관광지 개수만큼 마커들이 렌더링된다.', async () => {
+    render(
+      <KakaoMap
+        position={{
+          lat: 37.4737835,
+          lon: 126.9637296
+        }}
+        places={response1.response.body.items.item as unknown as PlaceType[]}
+        isLoading={false}
+      />
+    );
+    const kakaoMap = screen.getByTestId('kakao-map');
+    expect(kakaoMap).toBeInTheDocument();
 
-  // it('마커를 언호버하면 장소의 정보가 사라진다.', () => {});
+    const marker = await screen.findAllByRole('presentation');
+    expect(marker).toHaveLength(response1.response.body.items.item.length);
+  });
+  test('마커를 호버하면 장소의 정보가 보인다.', async () => {
+    render(
+      <KakaoMap
+        position={{
+          lat: 37.4737835,
+          lon: 126.9637296
+        }}
+        places={response1.response.body.items.item as unknown as PlaceType[]}
+        isLoading={false}
+      />
+    );
+    const kakaoMap = screen.getByTestId('kakao-map');
+    expect(kakaoMap).toBeInTheDocument();
+    const marker = await screen.findAllByRole('presentation');
+    expect(marker).toHaveLength(response1.response.body.items.item.length);
+    fireEvent.mouseOver(marker[0]);
+
+    const infoWindow = screen.getByText(new RegExp(response1.response.body.items.item[0].title, 'i'));
+    expect(infoWindow).toBeInTheDocument();
+  });
+
+  test('마커를 언호버하면 장소의 정보가 사라진다.', async () => {
+    render(
+      <KakaoMap
+        position={{
+          lat: 37.4737835,
+          lon: 126.9637296
+        }}
+        places={response1.response.body.items.item as unknown as PlaceType[]}
+        isLoading={false}
+      />
+    );
+    const kakaoMap = screen.getByTestId('kakao-map');
+    expect(kakaoMap).toBeInTheDocument();
+    const marker = await screen.findAllByRole('presentation');
+    expect(marker).toHaveLength(response1.response.body.items.item.length);
+    fireEvent.mouseOver(marker[0]);
+
+    const infoWindow = screen.getByText(new RegExp(response1.response.body.items.item[0].title, 'i'));
+    expect(infoWindow).toBeInTheDocument();
+
+    fireEvent.mouseOut(marker[0]);
+    expect(infoWindow).not.toBeInTheDocument();
+  });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -27,10 +27,44 @@ beforeAll(() => {
         return {};
       }),
       InfoWindow: vi.fn(),
+
       MarkerImage: vi.fn(),
-      Marker: vi.fn(),
+      Marker: vi.fn().mockImplementation((marker: { title: string }) => {
+        const _map = document.querySelector('#_map_4d7d48');
+
+        const markerImgWrapper = document.createElement('div');
+        const markerImg = document.createElement('img');
+        markerImg.setAttribute('src', 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png');
+        markerImg.setAttribute('role', 'presentation');
+        markerImgWrapper.appendChild(markerImg);
+
+        markerImgWrapper.addEventListener('mouseover', () => {
+          const infoWindow = document.createElement('div');
+          infoWindow.classList.add('_info_1p1je_1');
+          infoWindow.textContent = marker.title;
+          // ! markerIngWrapper 하위는 아니지만 테스트 목적은 infowWindow확인이라 편의상 markerImgWrapper 하위에 넣음
+          markerImgWrapper.appendChild(infoWindow);
+        });
+        _map?.appendChild(markerImgWrapper);
+
+        markerImgWrapper.addEventListener('mouseout', () => {
+          const infoWindow = document.querySelector('._info_1p1je_1');
+          if (infoWindow) {
+            infoWindow.remove();
+          } else {
+            throw new Error('마커에 마우스를 올렸는데 infoWindow가 안생긴 것 같음');
+          }
+        });
+        _map?.appendChild(markerImgWrapper);
+      }),
+
       event: {
-        addListener: vi.fn()
+        // addListener: vi.fn()
+        addListener: vi.fn().mockImplementation((target: 'marker' | 'map', eventName: string, callback: any) => {
+          if (eventName === 'mouseover' || target === 'marker') {
+            document.createElement('div').addEventListener('mouseover', callback);
+          }
+        })
       }
     }
   };
