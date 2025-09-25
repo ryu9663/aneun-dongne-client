ddc1bb7e683f8413acecd763bd3496447dbb238a
ryujunyeol <ryu9663@naver.com>
2025-09-25 16:44:52 +0900

    api 만료 대응

--- src/query/queryFn.ts
+++ src/query/queryFn.ts
@@ -5,7 +5,7 @@ export const getPlaces = async (params: PlaceParams): Promise<any> => {
   const { numOfRows, mapX, mapY, radius } = params;
   const data: Promise<any> = await axios
     .get(
-      `${import.meta.env.VITE_APP_TOUR_API_URL}/locationBasedList1?ServiceKey=${
+      `${import.meta.env.VITE_APP_TOUR_API_URL}/locationBasedList2?ServiceKey=${
         import.meta.env.VITE_APP_TOUR_API_KEY
       }&_type=json`,
       {
@@ -29,8 +29,8 @@ export const getPlaces = async (params: PlaceParams): Promise<any> => {
           //! 반경 몇m??
           radius,
           //*
-          arrange: 'A', // 정렬구분 (A=제목순, C=수정일순, D=생성일순) 대표이미지가반드시있는정렬(O=제목순, Q=수정일순, R=생성일순)
-          listYN: 'Y' // 목록구분(Y=목록, N=개수)
+          arrange: 'A' // 정렬구분 (A=제목순, C=수정일순, D=생성일순) 대표이미지가반드시있는정렬(O=제목순, Q=수정일순, R=생성일순)
+          // listYN: 'Y' // 목록구분(Y=목록, N=개수)
         },
         headers: { 'content-type': 'application/json' }
       }
