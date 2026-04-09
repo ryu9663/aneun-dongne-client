8f1ac39171a0e67195efd9492ef94aa444939ba8
류준열 <ryu9663@naver.com>
2026-04-09 13:07:13 +0900

    chore: add eslint autofix for unused imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,11 +9,23 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prefer-arrow'],
+  plugins: ['react-refresh', 'prefer-arrow', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'arrow-body-style': ['error', 'as-needed'],
     'react-refresh/only-export-components': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
 
     'prefer-arrow-callback': ['error', { allowNamedFunctions: true, allowUnboundThis: false }],
     'prefer-arrow/prefer-arrow-functions': [
